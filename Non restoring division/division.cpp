1da5c1d1289b71aea9cb04bb7955c980fda06781
robert <robspawlas@gmail.com>
2015-11-05 17:36:17 +0100

    Division template.

---
+++ Non restoring division/division.cpp
@@ -0,0 +1,237 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+const int NUM_SIZE(4);
+
+struct Bignum
+{
+    vector<bool> bin_num;
+    bool last_popped;
+    int size;
+
+    Bignum()
+    {}
+
+    Bignum(int si)
+    {
+        bin_num.resize(si);
+        size = si;
+        for(int i = 0; i != size; i++)
+        { bin_num[i] = false; }
+    }
+
+    ~Bignum()
+    {
+        size = 0;
+        last_popped = 0;
+    }
+
+    Bignum operator+(Bignum other)
+    {
+        Bignum result(other.size);
+        bool carry = false;
+        bool partial_sum = false;
+        bool sum = false;
+        for(int i = other.size - 1; i > -1; i--)
+        {
+            partial_sum = ((bin_num[i] != other.bin_num[i]));
+            sum = (partial_sum != carry);
+            carry = ((bin_num[i] && other.bin_num[i]) || (partial_sum && carry));
+            result.bin_num[i] = sum;
+         }
+        return result;
+    }
+
+    void operator<<(Bignum other)
+    {
+        Bignum result_this(size);
+        Bignum result_other(other.size);
+
+        for(int i = 0; i != size - 1; i++)
+        { result_this.bin_num[i] = bin_num[i + 1]; }
+
+        result_this.bin_num[size - 1] = other.bin_num[0];
+        last_popped = bin_num[0];
+
+        cout<<"remainder: ";
+        for(int i = 0; i != result_this.size; i++)
+            { cout<<result_this.bin_num[i]; }
+        cout<<endl;
+
+        for(int i = 0; i != size; i++)
+        { bin_num[i] = result_this.bin_num[i]; }
+
+        for(int i = 0 ; i != other.size - 1; i++)
+        { result_other.bin_num[i] = other.bin_num[i + 1]; }
+        result_other.bin_num[other.size - 1] = false;
+
+        for(int i = 0; i != other.size; i++)
+        { other.bin_num[i] = result_other.bin_num[i]; }
+    }
+
+    void operator<<(bool inserted)
+    {
+        Bignum result(size);
+
+        for(int j = 0; j != size - 1; j++)
+        { result.bin_num[j] = bin_num[j + 1]; }
+
+        result.bin_num[size - 1] = inserted;
+        last_popped = bin_num[0];
+
+        for(int i = 0; i != size; i++)
+        { bin_num[i] = result.bin_num[i]; }
+    }
+};
+
+struct Divis
+{
+    Bignum dividend;
+    Bignum divisor;
+    Bignum quotient;
+    Bignum remainder;
+    Bignum mantissa;
+
+    Divis() : dividend(NUM_SIZE), divisor(NUM_SIZE + 1), quotient(NUM_SIZE), remainder(NUM_SIZE + 1)
+    {}
+
+    void convert_and_load(char* divid, char* divis)
+    {
+        for(int i = 0; i != NUM_SIZE; i++)
+        {
+            divid[i] == '1' ? dividend.bin_num[i] = true : dividend.bin_num[i] = false;
+            divis[i] == '1' ? divisor.bin_num[i + 1] = true : divisor.bin_num[i + 1] = false;
+        }
+        divisor.bin_num[0] = false;
+
+        init_remainder();
+        init_quotient();
+    }
+
+    void init_remainder()
+    {
+        for(int i = 0; i != NUM_SIZE + 1; i++)
+            { remainder.bin_num[i] = false; }
+    }
+
+    void init_quotient()
+    {
+        for(int i = 0; i != NUM_SIZE; i++)
+        { quotient.bin_num[i] = dividend.bin_num[i]; }
+    }
+
+    void init_mantissa()
+    {
+        for(int i = 0; i != divisor.size; i++)
+        { mantissa.bin_num[i] = divisor.bin_num[i]; }
+    }
+
+    void divide()
+    {
+        Bignum divisor_compli = compliment(divisor);
+        Bignum mantissa = divisor_compli;
+
+        for(int i = 0; i != NUM_SIZE; i++)
+        {
+            remainder<<quotient;
+
+            if(remainder.last_popped == true)
+            { mantissa = divisor; }
+            else
+            { mantissa = divisor_compli; }
+            remainder = remainder + mantissa;
+            quotient.bin_num[quotient.size - 1] = !remainder.bin_num[0];
+        }
+        if(remainder.bin_num[0] == true)
+        { remainder = remainder + divisor; }
+    }
+
+    Bignum compliment(Bignum big_number)
+    {
+        Bignum result(big_number.size);
+        Bignum compliment_one(big_number.size);
+
+        for(int i = 0; i != big_number.size; i++)
+        {
+            result.bin_num[i] = !big_number.bin_num[i];
+            compliment_one.bin_num[i] = false;
+        }
+
+        compliment_one.bin_num[big_number.size - 1] = true;
+        result =  result + compliment_one;
+        return result;
+    }
+
+    char* shift(char* div)
+    {
+        int stop_index = 0;
+        for(int i = 0; i != NUM_SIZE + 1; i++)
+        {
+            if(div[i] == '\0')
+            { stop_index = i; }
+        }
+
+        int shift = NUM_SIZE - stop_index + 1;
+        char char_tmp[NUM_SIZE];
+
+        for(int i = 0; i != stop_index; i++)
+        { char_tmp[i] = div[i]; }
+
+        for(int i = 0; i != stop_index; i++)
+        { div[i + shift] = char_tmp[i]; }
+
+        for(int i = 0; i != shift; i++)
+        { div[i] = '0'; }
+
+        return div;
+    }
+
+    void result_print()
+    {
+        cout<<"Quotient: ";
+        for(int i = 0; i != quotient.size; i++)
+            { cout<<quotient.bin_num[i]; }
+        cout<<endl;
+
+        cout<<"Remainder: ";
+        for(int i = 0; i != remainder.size; i++)
+            { cout<<remainder.bin_num[i]; }
+        cout<<endl;
+    }
+};
+
+int main( int argc, const char* argv[] )
+{
+    char* dividend = new char[NUM_SIZE + 1];
+    char* divisor = new char[NUM_SIZE + 1];
+
+    char* holder1;
+    char* holder2;
+
+    for(int i = 0; i != NUM_SIZE + 1; i++)
+    {
+        dividend[i] = '0';
+        divisor[i] = '0';
+    }
+
+    cout<<"This is xxxxxxxx divisor"<<endl;
+    cout<<"Enter dividend: ";
+    cin>>dividend;
+    cout<<"Enter divisor: ";
+    cin>>divisor;
+
+    Divis divis;
+    holder1 = divis.shift(dividend);
+    holder2 = divis.shift(divisor);
+
+    divis.convert_and_load(holder1, holder2);
+
+    divis.divide();
+    divis.result_print();
+    delete []dividend;
+    delete []divisor;
+    delete holder1;
+    delete holder2;
+}
